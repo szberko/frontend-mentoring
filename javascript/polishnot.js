f2cee7ad3cbd58cce1b5eace719d4a419c617020
Szabolcs Berko <szberko@gmail.com>
2019-03-09 17:29:50 +0100

    * currently function version

--- javascript/polishnot.js
+++ javascript/polishnot.js
@@ -3,9 +3,24 @@
 var input = "+ 3 + 4 / 20 4";
 var symbolArray = getSymbolArray(input);
 
+const definedOperationPriority = {
+    "/" : 1,
+    "*" : 1,
+    "-" : 2,
+    "+" : 2
+}
 
-function Operation(operation, leftOperand, rightOperand){
+const definedOperations = {
+    "*" : (leftOperand, rightOperand) => Number.parseInt(leftOperand) * Number.parseInt(rightOperand),
+    "/" : (leftOperand, rightOperand) => Number.parseInt(leftOperand) / Number.parseInt(rightOperand),
+    "-" : (leftOperand, rightOperand) => Number.parseInt(leftOperand) - Number.parseInt(rightOperand),
+    "+" : (leftOperand, rightOperand) => Number.parseInt(leftOperand) + Number.parseInt(rightOperand)
+}
+
+
+function Operation(operation, operationPriority, leftOperand, rightOperand){
     this.operation = operation;
+    this.operationPriority = operationPriority;
     this.leftOperand = leftOperand;
     this.rightOperand = rightOperand;
 }
@@ -20,12 +35,14 @@ function compute(input){
         return;
     }
 
-    let operand;
+    let operation;
     let leftOperand;
     let rightOperand;
+    let operationPriority;
 
     if(input.length != 1){
-        operand = input[0];
+        operation = input[0];
+        operationPriority = definedOperationPriority[operation];
         input.shift();
         leftOperand = input[0];
         input.shift();
@@ -34,10 +51,38 @@ function compute(input){
         rightOperand = input[0];
     }
     
-    return new Operation(operand, leftOperand, rightOperand);
+    return new Operation(operation, operationPriority, leftOperand, rightOperand);
 }
 
-var operation = compute(symbolArray, operation);
+var currentOpLevel = 1;
 
-console.log(operation);
+function resolve(operationTree){
+    let oneLevelDeeperOp;
+    if(operationTree.operation == undefined){
+        oneLevelDeeperOp = operationTree;
+    }else{
+        oneLevelDeeperOp = resolve(operationTree.rightOperand);
+        
+        if(operationTree.operationPriority == currentOpLevel){
+            let currentOperation = definedOperations[operationTree.operation];
+            let calculatedValue = currentOperation(operationTree.leftOperand, operationTree.rightOperand.rightOperand);
+            operationTree.rightOperand = calculatedValue;
+
+            oneLevelDeeperOp = operationTree;
+        }
+
+        
+    }
+    return oneLevelDeeperOp;
+}
+
+var operationTree = compute(symbolArray, operationTree);
+
+console.log(operationTree);
+
+var operationTree1 = resolve(operationTree);
+currentOpLevel++;
+var operationTree2 = resolve(operationTree1);
+
+console.log(operationTree2);
 
