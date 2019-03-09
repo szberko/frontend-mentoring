a055fd60ee15ad76acfa36d225685daa2428268a
Szabolcs Berko <szberko@gmail.com>
2019-03-09 21:28:22 +0100

    add more documentation

--- javascript/task2.js
+++ javascript/task2.js
@@ -1,12 +1,16 @@
 function convertQueryToMap(query){
+    // Return empty object if there is no param
     let result = {};
 
+    // If we have multiple params
     if(query.includes("&")){
         let initialValue = {};
+        // Split request into multiple parameters
         result = query.split("&").reduce( (outerAggregator, currentPair) => {
+            // Split one parameter's key and value part
             let parsedPair = currentPair.split("=")
                                         .map(decodeURI);
-
+            // Split and walk through the key if the dot notation is used
             parsedPair[0].split(".").reduce( (innerAgregator, key, currentIndex, array) => {
                 if(!(key in innerAgregator)){
                     // If we are at the end of the path set the field to it's actual value
@@ -25,8 +29,9 @@ function convertQueryToMap(query){
             
             return outerAggregator;
         }, initialValue);
-
-    }else{
+    }
+    // If we have just a single param
+    else{
         result = [query].reduce( (resultingObj, currentPair) => {
             let parsedPair = currentPair.split("=")
                                         .map(decodeURI);
@@ -46,9 +51,17 @@ console.log("Criteria 1: ")
 console.log(prettyPrint(handleEmptyStringResult));
 
 var handleURIEncodingResult = convertQueryToMap("a=a%26b%3Dc%3F");
-console.log("Criteria 2:\n")
+console.log("Criteria 2:")
 console.log(prettyPrint(handleURIEncodingResult, null, 2));
 
 var handleBasicNestedComponents = convertQueryToMap("user.name.firstname=Bob&user.name.lastname=Smith&user.favoritecolor=Light%20Blue");
 console.log("Criteria 3:")
 console.log(prettyPrint(handleBasicNestedComponents, null, 2));
+
+var handleRepeatedPropertyNames = convertQueryToMap("user.1.name=Alice&user.2.name=Bob&user.3.name=Charles&user.4.name=Debbie");
+console.log("Criteria 4:")
+console.log(prettyPrint(handleRepeatedPropertyNames, null, 2));
+
+var custom = convertQueryToMap("firstname=Bob&lastname=Smith&favoritecolor=Light%20Blue");
+console.log("Criteria Custom:")
+console.log(prettyPrint(custom, null, 2));
