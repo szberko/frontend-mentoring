b835803c41ef5b72448f6c4fae697f212d1a8c03
Szabolcs Berko <szberko@gmail.com>
2019-03-16 22:59:54 +0100

    some styling change

--- todo/js/app.js
+++ todo/js/app.js
@@ -3,19 +3,12 @@
 import {Todo} from './todo.js';
 import {ListOfTodos} from './listoftodos.js'
 
-
-
 var todoItems = new ListOfTodos([
-        new Todo("Milk", 1, false),
-        new Todo("Eggs", 2, false),
-        new Todo("Sugar", 3, false),
-        new Todo("Carrots", null, true)
-    ])
-
-
-
-
-
+    new Todo("Milk", 1, false),
+    new Todo("Eggs", 2, false),
+    new Todo("Sugar", 3, false),
+    new Todo("Carrots", null, true)
+])
 
 window.onload = function() {
     todoItems.updateTheList();
