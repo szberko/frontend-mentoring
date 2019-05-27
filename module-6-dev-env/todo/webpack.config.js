6499f701ca543348b2e77c862873b548c2318649
Szabolcs Berko <szberko@gmail.com>
2019-05-27 22:10:18 +0200

    add inline-source-map as devtool to link bundled code to source code

--- module-6-dev-env/todo/webpack.config.js
+++ module-6-dev-env/todo/webpack.config.js
@@ -6,6 +6,7 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
     mode: 'development',
+    devtool: 'inline-source-map',
     entry: {
         app: './src/index.js'
     },
